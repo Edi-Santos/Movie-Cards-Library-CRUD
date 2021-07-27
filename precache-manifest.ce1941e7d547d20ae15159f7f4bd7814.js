056321dc8bb990bd330cc7ee5b1d345a5d43da41
Ed Santos <edsantos2822@gmail.com>
2021-07-27 04:44:59 -0300

    Updates

--- precache-manifest.ce1941e7d547d20ae15159f7f4bd7814.js
+++ precache-manifest.ce1941e7d547d20ae15159f7f4bd7814.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e902cb8597c2f8e907d3f7135bc98c5e",
+    "revision": "6eeeb643a6e4e4526c4fed32da8a305e",
     "url": "/Movie-Cards-Library-CRUD/index.html"
   },
   {
-    "revision": "7a196965e8f526e97e40",
-    "url": "/Movie-Cards-Library-CRUD/static/css/main.0ddd0c97.chunk.css"
+    "revision": "c9a58d66d51ed02fe875",
+    "url": "/Movie-Cards-Library-CRUD/static/css/main.ce255d47.chunk.css"
   },
   {
     "revision": "ff9021e4eeb56dae090e",
     "url": "/Movie-Cards-Library-CRUD/static/js/2.4248905f.chunk.js"
   },
   {
-    "revision": "7a196965e8f526e97e40",
-    "url": "/Movie-Cards-Library-CRUD/static/js/main.ed381bd1.chunk.js"
+    "revision": "c9a58d66d51ed02fe875",
+    "url": "/Movie-Cards-Library-CRUD/static/js/main.14e3aff7.chunk.js"
   },
   {
     "revision": "e2be16cb77c04413e511",
